f92104ffe8073baefacae7a839c85d4420c70892
Mahdi <mahditanzim2@gmail.com>
2023-09-21 17:56:15 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import Sidebar from "@/components/Sidebar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div
+      className="bg-black min-h-screen text-[#E7E9EA]
+    max-w-[1400px] mx-auto
+    
+    "
+    >
+      <Sidebar />
+      {/* <PostsFeed /> */}
+      {/* <Trending /> */}
+    </div>
+  );
+}
