0a144df7c7d5aba788f220b1b4667a4cd86872e4
Mahdi <mahditanzim2@gmail.com>
2023-09-21 21:39:41 -0400

    modal handling in progress

--- components/Trending.js
+++ components/Trending.js
@@ -1,8 +1,9 @@
 import { DotsHorizontalIcon, SearchIcon } from "@heroicons/react/outline";
+import { BadgeCheckIcon } from "@heroicons/react/solid";
 
 export default function Trending() {
   return (
-    <div className=" lg:flex">
+    <div className="hidden lg:flex flex-col ml-7 mt-4">
       Trending
       <div
         className="flex space-x-3 bg-white bg-opacity-10
@@ -18,7 +19,7 @@ export default function Trending() {
         />
       </div>
       <div
-        className="w-[300px h-[500px] bg-white bg-opacity-10
+        className="w-[300px] h-[500px] bg-white bg-opacity-10
             rounded-3xl mt-3"
       >
         <h1 className="font-bold text-xl p-3">What's happening</h1>
@@ -51,6 +52,64 @@ export default function Trending() {
           <p className="text-xs text-gray-500">Trending in US</p>
           <h1 className="text-[15px] font-bold">China</h1>
           <p className="text-xs text-gray-500">340K Tweets</p>
+        </div>
+      </div>
+      <div
+        className="w-[300px] h-[300px] bg-white bg-opacity-10
+            rounded-3xl mt-3"
+      >
+        <h1 className="font-bold text-xl p-3">Who to follow</h1>
+
+        <div className="flex justify-between p-3">
+          <div className="flex space-x-3">
+            <img
+              className="w-11 h-11 object-cover rounded-full"
+              src="/assets/bragg.png"
+            />
+            <div>
+              <div className="flex space-x-1">
+                <h1 className="font-bold">David Bragg</h1>
+                <BadgeCheckIcon className="w-[18px] text-blue-400" />
+              </div>
+              <h1 className="text-[12px] mt-1 text-gray-500">@davidbragg</h1>
+            </div>
+          </div>
+
+          <button className="bg-white text-black text-sm w-20 h-8 rounded-3xl font-bold">Follow</button>
+        </div>
+        <div className="flex justify-between p-3">
+          <div className="flex space-x-3">
+            <img
+              className="w-11 h-11 object-cover rounded-full"
+              src="/assets/bragg.png"
+            />
+            <div>
+              <div className="flex space-x-1">
+                <h1 className="font-bold">David Bragg</h1>
+                <BadgeCheckIcon className="w-[18px] text-blue-400" />
+              </div>
+              <h1 className="text-[12px] mt-1 text-gray-500">@davidbragg</h1>
+            </div>
+          </div>
+
+          <button className="bg-white text-black text-sm w-20 h-8 rounded-3xl font-bold">Follow</button>
+        </div>
+        <div className="flex justify-between p-3">
+          <div className="flex space-x-3">
+            <img
+              className="w-11 h-11 object-cover rounded-full"
+              src="/assets/bragg.png"
+            />
+            <div>
+              <div className="flex space-x-1">
+                <h1 className="font-bold">David Bragg</h1>
+                <BadgeCheckIcon className="w-[18px] text-blue-400" />
+              </div>
+              <h1 className="text-[12px] mt-1 text-gray-500">@davidbragg</h1>
+            </div>
+          </div>
+
+          <button className="bg-white text-black text-sm w-20 h-8 rounded-3xl font-bold">Follow</button>
         </div>
       </div>
     </div>
---
+++ components/modals/SignupModal.js
@@ -0,0 +1,28 @@
+import Modal from "@mui/material/Modal";
+import { useState } from "react";
+
+export default function SignupModal() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClose = () => setIsOpen(false);
+  const handleOpen = () => setIsOpen(true);
+  return (
+    <>
+      <button
+        className="bg-white text-black
+                w-[160px] rounded-full h-[40px] hover:bg-[#cbd2d7]
+                "
+                onClick={handleOpen}
+      >
+        Sign Up
+      </button>
+
+      <Modal
+        open={isOpen}
+        onClose={handleClose}
+        className="flex justify-center item-center"
+      >
+        <div className="w-[400px] h-[200px] bg-white">Sign up over here</div>
+      </Modal>
+    </>
+  );
+}
