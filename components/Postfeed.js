02438509ede430446fecc933e912b7c30ba4334b
Mahdi <mahditanzim2@gmail.com>
2023-09-27 09:00:06 -0400

    removed sample tweet

--- components/Postfeed.js
+++ components/Postfeed.js
@@ -35,7 +35,7 @@ export default function PostFeed() {
         return <Tweet key={tweet.id} id={tweet.id} data={tweet.data()} />;
       })}
 
-      <Tweet />
+      {/* <Tweet /> */}
     </div>
   );
 }
